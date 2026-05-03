51fa6a6b4b883859640fa253ad702ebc195be2da
tatrafin <47804737+tatrafin@users.noreply.github.com>
2026-05-03 14:03:15 -0400

    migration to newer django

--- collector/urls.py
+++ collector/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from collector import views
 
 urlpatterns = [
-    url(r'^log/$', views.log, name='log'),
+    path('log/', views.log, name='log'),
 ]
 
 
--- recommender/urls.py
+++ recommender/urls.py
@@ -1,29 +1,17 @@
-from django.conf.urls import url
+from django.urls import path
 
 from recommender import views
 
 urlpatterns = [
-    url(r'^chart/', views.chart, name='chart'),
-    url(r'^association_rule/(?P<content_id>\w+)/$',
-        views.get_association_rules_for,
-        name='get_association_rules_for'),
-    url(r'^ar/(?P<user_id>\w+)/$',
-        views.recs_using_association_rules,
-        name='recs_using_association_rules'),
-    url(r'^sim/user/(?P<user_id>\w+)/(?P<sim_method>\w+)/$',
-        views.similar_users, name='similar_users'),
-    url(r'^cb/item/(?P<content_id>\w+)/$',
-        views.similar_content, name='similar_content'),
-    url(r'^cb/user/(?P<user_id>\w+)/$',
-        views.recs_cb, name='recs_cb'),
-    url(r'^cf/user/(?P<user_id>\w+)/$',
-        views.recs_cf, name='recs_cb'),
-    url(r'^funk/user/(?P<user_id>\w+)/$',
-        views.recs_funksvd, name='recs_funksvd'),
-    url(r'^fwls/user/(?P<user_id>\w+)/$',
-        views.recs_fwls, name='recs_fwls'),
-    url(r'^bpr/user/(?P<user_id>\w+)/$',
-        views.recs_bpr, name='recs_fwls'),
-    url(r'^pop/user/(?P<user_id>\w+)/$',
-        views.recs_pop, name='recs_pop')
+    path('chart/', views.chart, name='chart'),
+    path('association_rule/<str:content_id>/', views.get_association_rules_for, name='get_association_rules_for'),
+    path('ar/<str:user_id>/', views.recs_using_association_rules, name='recs_using_association_rules'),
+    path('sim/user/<str:user_id>/<str:sim_method>/', views.similar_users, name='similar_users'),
+    path('cb/item/<str:content_id>/', views.similar_content, name='similar_content'),
+    path('cb/user/<str:user_id>/', views.recs_cb, name='recs_cb'),
+    path('cf/user/<str:user_id>/', views.recs_cf, name='recs_cb'),
+    path('funk/user/<str:user_id>/', views.recs_funksvd, name='recs_funksvd'),
+    path('fwls/user/<str:user_id>/', views.recs_fwls, name='recs_fwls'),
+    path('bpr/user/<str:user_id>/', views.recs_bpr, name='recs_fwls'),
+    path('pop/user/<str:user_id>/', views.recs_pop, name='recs_pop'),
 ]
